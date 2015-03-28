be9ead82a8ad0ad4e02c3b4c8d209eb5327488b6
NuclearBanane <rodger.waldron@gmail.com>
2015-03-28 13:42:05 -0400

    Added C and assumptions

--- queries.sql
+++ queries.sql
@@ -4,7 +4,13 @@
  */
 
 /*4. a*/
-SELECT * FROM Location L WHERE L.restaurant = "input";
+SELECT * FROM Location L WHERE L.restaurant = 'input';
 
-/*4.b*/
-SELECT * SORTED  FROM MenuItems I WHERE I.restaurantID = "input" 
+/*4. b*/
+SELECT * ORDER BY I.category FROM MenuItems I WHERE I.restaurantID = 'input';
+
+/*4. c*/
+
+SELECT L.Manager, L.FirstOpen FROM Location WHERE L.category='input';
+
+/*4. d*/
