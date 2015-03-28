2318895ace7e88dcc27a0d4de062cf11f1729949
NuclearBanane <rodger.waldron@gmail.com>
2015-03-28 13:29:59 -0400

    Fixed title page and started making queries.sql

---
+++ queries.sql
@@ -0,0 +1,10 @@
+/*
+ * Author : Rodger Waldron & Raj 
+ * 
+ */
+
+/*4. a*/
+SELECT * FROM Location L WHERE L.restaurant = "input";
+
+/*4.b*/
+SELECT * SORTED  FROM MenuItems I WHERE I.restaurantID = "input" 
