4e5277b36467864b313d107968236607265d9dfc
NuclearBanane <rodger.waldron@gmail.com>
2015-04-11 15:31:37 -0400

    Built a new Sign up and login page.
    Visitors will now hi9t the splash page which offers them a chance to signup or view the website without loging in.
    If they do view as a visitor (guest) then they will not see a user name in the top right but a signup button.
    
    This will bring them to the login page.
    
    Need to add "Not a member? Sign up here functionallity"
    
    Please note that none of this uses rigorous authentication. This trusts that the cookie has full integrity.
    This is simply because I wanted to make the work flow of the site and that part of the functionality first so some aspects of it are present.

--- restaurant.py
+++ restaurant.py
@@ -2,7 +2,7 @@
 # Author: NuclearBanane
 # Contributors : 
 # Date : 2015/02/12
-# Version : v0.1.2
+# Version : v0.2
 #####
 
 import tornado.ioloop   #Basic imports for the tornado library
@@ -17,39 +17,50 @@ class MainHandler(BaseHandler):
     def get(self):
         page = dict()
         #temporary to test Logins and cookies 
-        if not self.current_user:
-            self.redirect("/login")
+        if self.get_cookie("guestviewer")!='true':
+            self.redirect("/Splash")
             return
-        else: 
+        elif self.current_user : 
             page['authenticated']='true'
-
-        if not self.get_secure_cookie("mycookie"):
-            self.set_secure_cookie("mycookie", "myvalue")
-            #self.write("Your cookie was not set yet!")
-        #else:
-            #self.write("Your cookie was set!")
-
-        page['test']='Hi, this is a test page'
-        page['test2']='another great statement'
-        page['Name']= tornado.escape.xhtml_escape(self.current_user)
+            page['Name']= tornado.escape.xhtml_escape(self.current_user)
+        else :
+            page['authenticated']='false'
 
         self.render("assets/frontpage.html",mest=page) 
 
 class LoginHandler(BaseHandler):
     def get(self):
-        self.write('<html><body><form action="/login" method="post">'
+        self.write('<html><body><form action="/Login" method="post">'
                    'Name: <input type="text" name="name">'
                    '<input type="submit" value="Sign in">'
                    '</form></body></html>')
 
     def post(self):
         self.set_secure_cookie("user", self.get_argument("name"))
+        self.set_cookie("guestviewer", "true")
         self.redirect("/")
 
 class SignupHandler(BaseHandler):
     def get(self):
+                self.render("assets/signup.html") 
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("name"))
+        self.set_cookie("guestviewer", "true")
+        self.redirect("/")
+
+class SplashHandler(BaseHandler):
+    def get(self):
+        self.render("assets/splash.html")
+
+class GuestHandler(BaseHandler):
+    def get(self):
+        self.set_cookie("guestviewer", "true")
+        self.redirect("/")
         return
 
+class ProfileHandler(BaseHandler):
+    def get(self):
+        self.write('<html><body> <p>making this soon</p></body></html>')
 
 ####
 # Tornado uses 'handlers' to take care of requests to certain URLs
@@ -60,8 +71,11 @@ class SignupHandler(BaseHandler):
 application = tornado.web.Application(
 	[
 		(r'/',              MainHandler),
-        (r'/login',         LoginHandler),
+        (r'/Splash',        SplashHandler),
+        (r'/Login',         LoginHandler),
         (r'/Signup',        SignupHandler),
+        (r'/Guest',         GuestHandler),
+        (r'/Profile',       ProfileHandler),
 
 
 		(r'/(favicon.ico)', tornado.web.StaticFileHandler, {'path': 'assets/'        }),
